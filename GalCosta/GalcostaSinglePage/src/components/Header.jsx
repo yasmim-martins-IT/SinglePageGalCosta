cf53845c368a404d5ceb68329429cbb1e9a92660
Yasmim Sampaio Martins <53661815857@>
2025-08-15 15:59:25 -0300

    criando componente header

--- GalCosta/GalcostaSinglePage/src/components/Header.jsx
+++ GalCosta/GalcostaSinglePage/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from "./Header.module.css"
 import galcosta from '../assets/galcosta.jpg'
+import logo from '../assets/Spotify.png'
 
 function Header() {
   return (
     <header className={styles.general_config}>
-      <img src={galcosta} alt="imagem da galcosta" />
+      <img className={styles.logospotfy} src={logo} alt="imagem da galcosta" />
+
+      <div >
+        <ul className={styles.general_config}>
+            <li>principais sucessos</li>
+            <li>historia da cantora</li>
+        </ul>
+      </div>
     </header>
   );
 }
